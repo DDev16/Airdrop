08c2e69c480556d9c9bd409966bc18b3e6c5a43b
Dillon Devera <dillondevera12@gmail.com>
2024-06-04 19:01:08 -0700

    Add new

--- contracts/ERC20Multisender.sol
+++ contracts/ERC20Multisender.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Multisender {
-    function multisendToken(address token, address[] calldata recipients, uint256 amount) external {
+contract ERC20Multisender is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) {
+        // The ownership is transferred to initialOwner by the Ownable constructor
+    }
+
+    function multisendToken(address token, address[] calldata recipients, uint256 amount) external onlyOwner {
         IERC20 erc20 = IERC20(token);
         for (uint256 i = 0; i < recipients.length; i++) {
             require(erc20.transferFrom(msg.sender, recipients[i], amount), "Transfer failed");
         }
     }
 }
- 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Deploy the DogOnMoon contract
-  const DogOnMoon = await hre.ethers.getContractFactory("DogOnMoon");
-  const dogOnMoon = await DogOnMoon.deploy();
-  await dogOnMoon.waitForDeployment();
-  const dogOnMoonAddress = await dogOnMoon.getAddress();
-  console.log("DogOnMoon deployed to:", dogOnMoonAddress);
 
+  const initialOwner = "0xb77c6FA8bDe5372B0DfD94898bBfE57E5522B355"; // Replace with the actual owner address
+  
   // Deploy the ERC20Multisender contract
   const ERC20Multisender = await hre.ethers.getContractFactory("ERC20Multisender");
-  const multisender = await ERC20Multisender.deploy();
+  const multisender = await ERC20Multisender.deploy(initialOwner);
   await multisender.waitForDeployment();
   const multisenderAddress = await multisender.getAddress();
   console.log("ERC20Multisender deployed to:", multisenderAddress);
